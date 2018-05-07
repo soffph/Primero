5bedd0174c91dbfbf080e8d32a9d4de77da4dc58
soffph <soff.dg18@gmail.com>
2018-05-07 09:59:30 -0500

    examen conexiones

---
+++ ExamenCM/ExamenCM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExamenCM
+//
+//  Created by sofia delgado gonzalez on 5/6/18.
+//  Copyright © 2018 sofia delgado gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
